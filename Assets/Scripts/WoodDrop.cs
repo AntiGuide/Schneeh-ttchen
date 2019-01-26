0382f2bdfe2b1a63c814448bb4de91b856ee83d5
Lukas Wiedemann <lukaswiedemann@protonmail.com>
2019-01-26 14:05:37 +0100

    WoodSpawner.cs + WoodDrop.cs added

---
+++ Assets/Scripts/WoodDrop.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WoodDrop : MonoBehaviour {
+    public WoodSpawner ParentSpawner { get; set; }
+
+    public void PickUp() {
+        ParentSpawner.RemoveWoodDrop(gameObject);
+    }
+}
