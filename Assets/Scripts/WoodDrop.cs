4875949a87caaaf444f0d64e3b642fe1c53e9cf9
Lukas Wiedemann <lukaswiedemann@protonmail.com>
2019-01-26 14:42:05 +0100

    Added wood interaction

--- Assets/Scripts/WoodDrop.cs
+++ Assets/Scripts/WoodDrop.cs
@@ -2,10 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WoodDrop : MonoBehaviour {
-    public WoodSpawner ParentSpawner { get; set; }
+public class WoodDrop : MonoBehaviour, IInteractable {
+    public WoodSpawner ParentSpawner;
+    public float ChanceDoubleStack = 0.05f;
 
-    public void PickUp() {
-        ParentSpawner.RemoveWoodDrop(gameObject);
+    private States state = States.RAW;
+    private int stackCount;
+
+    private enum States {
+        RAW,
+        PROCESSED,
+    }
+
+    private void Start() {
+        stackCount = Random.value < ChanceDoubleStack ? 2 : 1;
+    }
+
+    public void Interact(PlayerInventory playerInventory) {
+        switch (state) {
+            case States.RAW:
+                state = playerInventory.playerHolds == PlayerInventory.HoldItems.AXE ? States.PROCESSED : state;
+                break;
+            case States.PROCESSED:
+                if (playerInventory.AddItem(PlayerInventory.Items.WOOD)) {
+                    stackCount--;
+                    if (stackCount == 0) {
+                        ParentSpawner.RemoveWoodDrop(gameObject);
+                        Destroy(gameObject);
+                    }
+                }
+                break;
+            default:
+                break;
+        }
     }
 }
